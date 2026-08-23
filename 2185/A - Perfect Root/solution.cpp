f2908f64aaf437cb3fe4732d4630fdd88cb14c2b
jahid8358 <jahidhasan8358@gmail.com>
2026-08-23 12:50:51 +0600

    Add Perfect Root [A] from Codeforces

---
+++ 2185/A - Perfect Root/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   
+   for(int i=1; i<=t; i++)
+   {
+      int b;
+      cin>>b;
+      for(int j=1; j<=b; j++)
+      {
+          cout<<" "<<j;
+      }
+       cout<<endl;
+   }
+    return 0;
+}
