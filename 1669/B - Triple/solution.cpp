65416fbf334ec8d83ee4f043981285e00212da41
jahid8358 <jahidhasan8358@gmail.com>
2026-08-23 13:08:24 +0600

    Add Triple [B] from Codeforces

---
+++ 1669/B - Triple/solution.cpp
@@ -0,0 +1,79 @@
+/*
+ *       ___      _       _   _   _   ____
+ *      |_  |    / \     | | | | | | |  _ \
+ *        | |   / _ \    | |_| | | | | | | |
+ *    | |_| |  / ___ \   |  _  | | | | |_| |
+ *     \___/  /_/   \_\  |_| |_| |_| |____/
+ *
+ *    Problem:  B_Triple
+ *    Time:     19-08-2026 22:26
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+// Typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a.begin(), a.end());
+    int ans = -1;
+    if (n < 3)
+        cout << ans << endl;
+    else
+    {
+        for (int i = 0; i < n - 2; i++)
+        {
+            if (a[i] == a[i + 1] && a[i + 1] == a[i + 2])
+            {
+                ans = a[i];
+                break;
+            }
+        }
+        cout << ans << endl;
+    }
+}
+ 
+int main()
+{
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
