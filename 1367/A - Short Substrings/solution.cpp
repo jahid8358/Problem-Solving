f5c5fcb8bcb240bce66507833608d2e9fbde195b
jahid8358 <jahidhasan8358@gmail.com>
2026-08-23 13:06:46 +0600

    Add Short Substrings [A] from Codeforces

---
+++ 1367/A - Short Substrings/solution.cpp
@@ -0,0 +1,67 @@
+/*
+ *       ___      _       _   _   _   ____
+ *      |_  |    / \     | | | | | | |  _ \
+ *        | |   / _ \    | |_| | | | | | | |
+ *    | |_| |  / ___ \   |  _  | | | | |_| |
+ *     \___/  /_/   \_\  |_| |_| |_| |____/
+ *
+ *    Problem:  A_Short_Substrings
+ *    Time:     13-08-2026 22:26
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+// Typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+void solve() // abbaac
+             // abac   0 1 3 5
+{
+    string s;
+    cin >> s;
+    string a = s.substr(0, 2);
+ 
+    for (int i = 3; i < s.length(); i += 2)
+        a += s[i];
+ 
+    cout << a << endl;
+}
+ 
+int main()
+{
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
