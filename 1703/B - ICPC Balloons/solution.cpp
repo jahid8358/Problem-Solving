2a489a8fb79c7120125080e60026cf5b1a0eb508
jahid8358 <jahidhasan8358@gmail.com>
2026-08-23 13:06:50 +0600

    Add ICPC Balloons [B] from Codeforces

---
+++ 1703/B - ICPC Balloons/solution.cpp
@@ -0,0 +1,78 @@
+/*
+ *       ___      _       _   _   _   ____
+ *      |_  |    / \     | | | | | | |  _ \
+ *        | |   / _ \    | |_| | | | | | | |
+ *    | |_| |  / ___ \   |  _  | | | | |_| |
+ *     \___/  /_/   \_\  |_| |_| |_| |____/
+ *
+ *    Problem:  B_ICPC_Balloons
+ *    Time:     14-08-2026 17:21
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+// Typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+ 
+    string s;
+    cin >> s;
+    int ans = 0;
+ 
+    bool a[26] = {false};
+ 
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (a[s[i] - 'A'] == false)
+        {
+            ans += 2;
+            a[s[i] - 'A'] = true;
+        }
+        else
+            ans++;
+    }
+    cout << ans << endl;
+}
+ 
+int main()
+{
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
