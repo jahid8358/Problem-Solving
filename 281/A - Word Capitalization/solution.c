ce1c7a7dea68160da26eb0949b20b05a5699b87d
jahid8358 <jahidhasan8358@gmail.com>
2026-08-23 12:49:35 +0600

    Add Word Capitalization [A] from Codeforces

---
+++ 281/A - Word Capitalization/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+ 
+int main() {
+    char word[1000];
+    scanf("%s", word);
+ 
+ 
+    if(word[0] >= 'a' && word[0] <= 'z') {
+        word[0] = word[0] - 'a' + 'A';
+    }
+ 
+    printf("%s
+", word);
+    return 0;
+}
