a2c49f6f3cd19734b59e56f2909e4fafc4d1463a
jahid8358 <jahidhasan8358@gmail.com>
2026-08-23 13:03:40 +0600

    Add Holiday Of Equality [A] from Codeforces

---
+++ 758/A - Holiday Of Equality/solution.cpp
@@ -0,0 +1,74 @@
+/*
+ *       ___      _       _   _   _   ____
+ *      |_  |    / \     | | | | | | |  _ \
+ *        | |   / _ \    | |_| | | | | | | |
+ *    | |_| |  / ___ \   |  _  | | | | |_| |
+ *     \___/  /_/   \_\  |_| |_| |_| |____/
+ *
+ *    Problem:  A_Holiday_Of_Equality
+ *    Time:     12-08-2026 01:40
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+// Typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+void solve()
+{
+    ll n;
+    cin >> n;
+ 
+    vi a(n);
+    for (ll i = 0; i < n; i++)
+        cin >> a[i];
+ 
+    sort(all(a));
+ 
+    ll ans = 0;
+ 
+    for (ll i = 0; i < n; i++)
+    {
+        ans += a[n - 1] - a[i];
+    }
+ 
+    cout << ans << endl;
+}
+ 
+int main()
+{
+    fastio();
+ 
+    int t = 1;
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
