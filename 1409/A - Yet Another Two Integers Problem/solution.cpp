f35f20dce0a052ae054e5b8a7c77c09fceae3043
jahid8358 <jahidhasan8358@gmail.com>
2026-08-23 13:03:24 +0600

    Add Yet Another Two Integers Problem [A] from Codeforces

---
+++ 1409/A - Yet Another Two Integers Problem/solution.cpp
@@ -0,0 +1,69 @@
+/*
+ *       ___      _       _   _   _   ____
+ *      |_  |    / \     | | | | | | |  _ \
+ *        | |   / _ \    | |_| | | | | | | |
+ *    | |_| |  / ___ \   |  _  | | | | |_| |
+ *     \___/  /_/   \_\  |_| |_| |_| |____/
+ *
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+// Typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+ 
+    int d = abs(a - b);
+ 
+    if (d == 0)
+        cout << "0
+";
+    else
+    {
+        int ans = (d + 10 - 1) / 10;
+        cout << ans << endl;
+    }
+}
+ 
+int main()
+{
+    fastio();
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
