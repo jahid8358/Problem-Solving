0713077b15ecdfddadff6afa59604cf776d8e3a7
jahid8358 <jahidhasan8358@gmail.com>
2026-08-23 13:03:54 +0600

    Add Dislike of Threes [A] from Codeforces

---
+++ 1560/A - Dislike of Threes/solution.cpp
@@ -0,0 +1,71 @@
+/*
+ *       ___      _       _   _   _   ____
+ *      |_  |    / \     | | | | | | |  _ \
+ *        | |   / _ \    | |_| | | | | | | |
+ *    | |_| |  / ___ \   |  _  | | | | |_| |
+ *     \___/  /_/   \_\  |_| |_| |_| |____/
+ *
+ *    Problem:  A_Dislike_of_Threes
+ *    Time:     12-08-2026 02:34
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+// Typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+void solve()
+{
+    int k;
+    cin >> k;
+    for (int i = 1;; i++)
+    {
+        if (i % 3 == 0 || i % 10 == 3)
+            continue;
+ 
+        if (--k == 0)
+        {
+            cout << i << endl;
+            break;
+        }
+    }
+}
+ 
+int main()
+{
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
