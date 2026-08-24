902b698176c47b718a4d045068a0abfc578ecae2
jahid8358 <jahidhasan8358@gmail.com>
2026-08-24 22:38:55 +0600

    Add Make it White [A] from Codeforces

---
+++ 1927/A - Make it White/solution.cpp
@@ -0,0 +1,80 @@
+/*
+ *       ___      _       _   _   _   ____
+ *      |_  |    / \     | | | | | | |  _ \
+ *        | |   / _ \    | |_| | | | | | | |
+ *    | |_| |  / ___ \   |  _  | | | | |_| |
+ *     \___/  /_/   \_\  |_| |_| |_| |____/
+ *
+ *    Problem:  A_Make_it_White
+ *    Time:     24-08-2026 20:47
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+// Typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int first_black = 0, last_black = n - 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == 'B')
+        {
+            first_black = i;
+            break;
+        }
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (s[i] == 'B')
+        {
+            last_black = i;
+            break;
+        }
+    }
+    cout << last_black - first_black + 1 << endl;
+}
+ 
+int main()
+{
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
