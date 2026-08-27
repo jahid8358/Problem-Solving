c660a91244ca211632f4e1fa017ece4b5fde3154
jahid8358 <jahidhasan8358@gmail.com>
2026-08-28 00:17:44 +0600

    Add Colourblindness [B] from Codeforces

---
+++ 1722/B - Colourblindness/solution.cpp
@@ -0,0 +1,78 @@
+/*
+ *       ___      _       _   _   _   ____
+ *      |_  |    / \     | | | | | | |  _ \
+ *        | |   / _ \    | |_| | | | | | | |
+ *    | |_| |  / ___ \   |  _  | | | | |_| |
+ *     \___/  /_/   \_\  |_| |_| |_| |____/
+ *
+ *    Problem:  B_Colourblindness
+ *    Time:     27-08-2026 23:54
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+// Typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    string a, b;
+    cin >> a >> b;
+    bool upper_row = false;
+    bool lower_row = false;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == 'G')
+            a[i] = 'B';
+        if (b[i] == 'G')
+            b[i] = 'B';
+    }
+    if (a == b)
+        cout << "YES
+";
+    else
+        cout << "NO
+";
+}
+ 
+int main()
+{
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
