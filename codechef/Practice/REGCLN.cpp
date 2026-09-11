73f175512dffd4a53dc0a02c04229e728e950da2
jahid8358 <jahidhasan8358@gmail.com>
2026-09-11 11:49:15 +0600

    Solved: REGCLN (codechef)

---
+++ codechef/Practice/REGCLN.cpp
@@ -0,0 +1,8 @@
+// Problem: REGCLN
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/problems/REGCLN?tab=ide
+// Solved on: 2026-09-11T05:49:26.295Z
+
+// source not captured automatically - open the solution page and copy it manually
