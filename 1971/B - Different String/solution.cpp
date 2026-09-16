387d4eede8db2a02c20fa7643dfe6054614f8e7b
jahid8358 <jahidhasan8358@gmail.com>
2026-09-16 09:29:22 +0600

    Add Different String [B] from Codeforces

---
+++ 1971/B - Different String/solution.cpp
@@ -0,0 +1,78 @@
+/*
+ *       ___      _       _   _   _   ____
+ *      |_  |    / \     | | | | | | |  _ \
+ *        | |   / _ \    | |_| | | | | | | |
+ *    | |_| |  / ___ \   |  _  | | | | |_| |
+ *     \___/  /_/   \_\  |_| |_| |_| |____/
+ *
+ *    Problem:  B_Different_String
+ *    Time:     16-09-2026 09:21
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+// Typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+void solve()
+{
+    string s;
+    cin >> s;
+    int len = s.length();
+ 
+    if (len < 2)
+        cout << "NO" << endl;
+    else
+    {
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            if (s[i] != s[i + 1])
+            {
+                swap(s[i], s[i + 1]);
+                cout << "YES" << endl;
+                cout << s << endl;
+                return;
+            }
+        }
+        cout << "NO" << endl;
+    }
+}
+ 
+int main()
+{
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
