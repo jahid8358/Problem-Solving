be79a9371d828bf7a9c64502285f95ac69d3223d
jahid8358 <jahidhasan8358@gmail.com>
2026-09-17 14:51:01 +0600

    Add Drinks [B] from Codeforces

---
+++ 200/B - Drinks/solution.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    cout << fixed << setprecision(12);
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        int x;
+        cin >> x;
+        sum += x;
+    }
+    cout << (double)sum / n << endl;
+    return 0;
+}
