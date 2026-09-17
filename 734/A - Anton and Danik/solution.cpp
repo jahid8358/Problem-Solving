c6a7e91975684bd181f6d66baa308a99d81ce3b3
jahid8358 <jahidhasan8358@gmail.com>
2026-09-17 14:50:23 +0600

    Add Anton and Danik [A] from Codeforces

---
+++ 734/A - Anton and Danik/solution.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+ 
+    int anton = 0, danik = 0;
+    for(int i=0; i<n; i++) {
+        if(s[i] == 'A')
+        anton++;
+        else
+        danik++;
+    }
+ 
+    if(anton == danik)
+    cout << "Friendship
+";
+    else if(anton > danik)
+    cout << "Anton
+";
+    else
+    cout << "Danik
+";
+ 
+    return 0;
+}
