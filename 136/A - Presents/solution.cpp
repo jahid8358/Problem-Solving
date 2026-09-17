1be4ab6166d8127d3bc1475a5d6e5e9384ea4635
jahid8358 <jahidhasan8358@gmail.com>
2026-09-17 14:51:08 +0600

    Add Presents [A] from Codeforces

--- 136/A - Presents/solution.cpp
+++ 136/A - Presents/solution.cpp
@@ -1,21 +1,28 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 using namespace std;
-int main()
-{
-  int n;
-  cin >> n;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
  
-  int arr[n + 1];
+    int x = 1;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(a[j] == x){
+                b[i] = j+1;
+                x++;
+                break;
+            }
+        }
+    }
  
-  for (int i = 1; i <= n; i++)
-  {
-    int x;
-    cin >> x;
-    arr[x] = i;
-  }
-  for (int i = 1; i <= n; i++)
-    cout << arr[i] << " ";
- 
-  cout << endl;
-  return 0;
+    for(int i=0; i<n; i++){
+        cout << b[i] << " ";
+    }
+    return 0;
 }
