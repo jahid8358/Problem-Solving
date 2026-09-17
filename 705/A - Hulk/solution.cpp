c014d19966979080dd21d9882d78812f0d7af3b1
jahid8358 <jahidhasan8358@gmail.com>
2026-09-17 14:50:31 +0600

    Add Hulk [A] from Codeforces

--- 705/A - Hulk/solution.cpp
+++ 705/A - Hulk/solution.cpp
@@ -1,22 +1,19 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
- 
-int main()
-{
-  int n;
-  cin >> n;
- 
-  for (int i = 1; i <= n; i++)
-  {
-    if (i % 2 != 0)
-      cout << "I hate ";
-    else
-      cout << "I love ";
- 
-    if (i == n)
-      cout << "it";
-    else
-      cout << "that ";
-  }
-  return 0;
+int main(){
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        if(i%2!=0 && i==n)
+        cout << "I hate it
+";
+        else if(i%2==0 && i==n)
+        cout << "I love it
+";
+        else if(i%2!=0 && i!=n)
+        cout << "I hate that ";
+        else if(i%2==0 && i!=n)
+        cout << "I love that ";
+    }
+    return 0;
 }
