7eea1962abe1733651e146478b4fb07cca3defd6
jahid8358 <jahidhasan8358@gmail.com>
2026-09-23 21:29:36 +0600

    Add Advantage [C] from Codeforces

---
+++ 1760/C - Advantage/solution.cpp
@@ -0,0 +1,78 @@
+/*
+ *       ___      _       _   _   _   ____
+ *      |_  |    / \     | | | | | | |  _ \
+ *        | |   / _ \    | |_| | | | | | | |
+ *    | |_| |  / ___ \   |  _  | | | | |_| |
+ *     \___/  /_/   \_\  |_| |_| |_| |____/
+ *
+ *    Problem:  C_Advantage
+ *    Time:     23-09-2026 21:25
+ */
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+ 
+// Macros
+#define rep(i, j) for (int i = 0; i < j; i++)
+#define rrep(i, j) for (int i = j - 1; i >= 0; i--)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl '
+'
+ 
+// Typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+// Constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+ 
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        b[i] = a[i];
+    }
+ 
+    sort(b.begin(), b.end());
+ 
+    for (int i = 0; i < n; i++)
+    {
+        if (b[n - 1] == a[i])
+            cout << a[i] - b[n - 2] << " ";
+        else
+            cout << a[i] - b[n - 1] << " ";
+    }
+    cout << endl;
+}
+ 
+int main()
+{
+    fastio();
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
